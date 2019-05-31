fe6ed69f987d50723fc5ad207febac3861f6fe97
masalthunlass <masalthunlass@gmail.com>
2019-05-31 15:02:35 +0200

    battle kata starting step

---
+++ src/main/java/battle/Game.java
@@ -0,0 +1,6 @@
+package battle;
+
+class Game {
+
+
+}
---
+++ src/test/java/battle/GameTest.java
@@ -0,0 +1,15 @@
+package battle;
+
+import org.junit.Test;
+
+public class GameTest {
+
+    private Game game;
+
+    @Test
+    public void should() {
+        //given
+        //when
+        //then
+    }
+}
