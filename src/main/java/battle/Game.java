9c602a301cf863e50dcd31b55c51600f6da9c7bb
masalthunlass <masalthunlass@gmail.com>
2019-05-31 15:32:17 +0200

    add passing test : should game start with two players

--- src/main/java/battle/Game.java
+++ src/main/java/battle/Game.java
@@ -1,6 +1,19 @@
 package battle;
 
 class Game {
+    private String playerOne, playerTwo;
 
+    void start(String playerOne, String playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
 
+    }
+
+    String getPlayerOne() {
+        return this.playerOne;
+    }
+
+    String getPlayerTwo() {
+        return this.playerTwo;
+    }
 }
--- src/test/java/battle/GameTest.java
+++ src/test/java/battle/GameTest.java
@@ -2,14 +2,24 @@ package battle;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GameTest {
 
-    private Game game;
+    private Game game = new Game();
 
     @Test
-    public void should() {
+    public void shouldGameStartsWithTwoPlayers() {
         //given
+        final String playerOne = "PlayerOne";
+        final String playerTwo = "PlayerTwo";
         //when
+        this.game.start(playerOne, playerTwo);
         //then
+        assertThat(this.game.getPlayerOne()).isNotNull();
+        assertThat(this.game.getPlayerTwo()).isNotNull();
+        assertThat(this.game.getPlayerOne()).isEqualTo(playerOne);
+        assertThat(this.game.getPlayerTwo()).isEqualTo(playerTwo);
+
     }
 }
